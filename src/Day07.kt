264fb03810c2786d3ae2c7a941fa1bd921737529
Sage Choi <sage@wonder.com>
2021-12-07 00:56:57 -0500

    sum of sequence (better than memoized recursion)

--- src/Day07.kt
+++ src/Day07.kt
@@ -14,14 +14,19 @@ fun main() {
         val pos = input.split(",").map { it.toInt() }
         val max = pos.maxOrNull()!!
 
-        val costMap: MutableMap<Int, Int> = mutableMapOf()
-        fun costStep(s: Int): Int {
-            if (s == 0) return 0
-            return costMap[s] ?: (s + costStep(s - 1)).also { costMap[s] = it }
-        }
-
+//        val costMap: MutableMap<Int, Int> = mutableMapOf()
+//        fun costStep(s: Int): Int {
+//            if (s == 0) return 0
+//            return costMap[s] ?: (s + costStep(s - 1)).also { costMap[s] = it }
+//        }
+//        return (0..max).fold(Int.MAX_VALUE) { minFuel, p ->
+//            min(minFuel, pos.sumOf { costStep(abs(it - p)) })
+//        }
         return (0..max).fold(Int.MAX_VALUE) { minFuel, p ->
-            min(minFuel, pos.sumOf { costStep(abs(it - p)) })
+            min(minFuel, pos.sumOf {
+                val d = abs(it - p)
+                d * (d + 1) / 2
+            })
         }
     }
 
