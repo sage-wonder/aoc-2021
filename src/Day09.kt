2b508b27d6b3d6871c05c15edc3aab12cd9e20e8
Sage Choi <sage@wonder.com>
2021-12-09 00:45:30 -0500

    use typealias

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,48 +1,47 @@
+typealias LavaFloor = List<List<Int>>
+typealias Point = Pair<Int, Int>
+
 fun main() {
-    fun List<String>.isLowPoint(x: Int, y: Int): Boolean {
-        val curr = this[x][y].digitToInt()
-        return (x == 0 || this[x - 1][y].digitToInt() > curr) &&
-                (x == this.size - 1 || this[x + 1][y].digitToInt() > curr) &&
-                (y == 0 || this[x][y - 1].digitToInt() > curr) &&
-                (y == this[0].length - 1 || this[x][y + 1].digitToInt() > curr)
+    fun LavaFloor.isLowPoint(p: Point): Boolean {
+        val (x, y) = p
+        val curr = this[x][y]
+        return (x == 0 || this[x - 1][y] > curr) &&
+                (x == this.size - 1 || this[x + 1][y] > curr) &&
+                (y == 0 || this[x][y - 1] > curr) &&
+                (y == this[0].size - 1 || this[x][y + 1] > curr)
     }
 
-    fun List<String>.basin(basinArea: MutableSet<Pair<Int, Int>>, x: Int, y: Int): Set<Pair<Int, Int>> {
-        if (x < 0 || x >= this.size || y < 0 || y >= this[0].length || this[x][y] == '9') return emptySet()
-        basinArea.add(x to y)
+    fun LavaFloor.basin(basinArea: MutableSet<Point>, p: Point): Set<Point> {
+        val (x, y) = p
+        if (x < 0 || x >= this.size || y < 0 || y >= this[0].size || this[x][y] == 9) return emptySet()
+        basinArea.add(p)
         if (!basinArea.contains(x - 1 to y))
-            basinArea.addAll(this.basin(basinArea, x - 1, y))
+            basinArea.addAll(this.basin(basinArea, x - 1 to y))
         if (!basinArea.contains(x + 1 to y))
-            basinArea.addAll(this.basin(basinArea, x + 1, y))
+            basinArea.addAll(this.basin(basinArea, x + 1 to y))
         if (!basinArea.contains(x to y - 1))
-            basinArea.addAll(this.basin(basinArea, x, y - 1))
+            basinArea.addAll(this.basin(basinArea, x to y - 1))
         if (!basinArea.contains(x to y + 1))
-            basinArea.addAll(this.basin(basinArea, x, y + 1))
+            basinArea.addAll(this.basin(basinArea, x to y + 1))
         return basinArea
     }
 
-    fun part1(input: List<String>): Int {
-        return (input.indices).flatMap { x ->
-            (0 until input[0].length).mapNotNull { y ->
-                if (input.isLowPoint(x, y)) input[x][y]
-                else null
-            }
-        }.sumOf { it.digitToInt() + 1 }
+    fun part1(input: LavaFloor): Int {
+        return (input.indices)
+            .flatMap { x -> (input[0].indices).mapNotNull { y -> if (input.isLowPoint(x to y)) input[x][y] else null } }
+            .sumOf { it + 1 }
     }
 
-    fun part2(input: List<String>): Int {
-        return (input.indices).flatMap { x ->
-            (0 until input[0].length).mapNotNull { y ->
-                if (input.isLowPoint(x, y)) x to y
-                else null
-            }
-        }.map { (x, y) -> input.basin(mutableSetOf(), x, y).size }
+    fun part2(input: LavaFloor): Int {
+        return (input.indices)
+            .flatMap { x -> (input[0].indices).mapNotNull { y -> if (input.isLowPoint(x to y)) x to y else null } }
+            .map { input.basin(mutableSetOf(), it).size }
             .sortedDescending()
             .take(3)
             .fold(1) { acc, basinSize -> acc * basinSize }
     }
 
-    val input = readInput("day9")
+    val input = readInput("day9").map { it.map { it.digitToInt() } }
     println(part1(input))
     println(part2(input))
 }
