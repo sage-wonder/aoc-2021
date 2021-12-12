4bb8ee799caa2ef261acb2f62d1c3584edd1ad0e
Sage Choi <sage@wonder.com>
2021-12-12 03:01:56 -0500

    day12

---
+++ src/Day12.kt
@@ -0,0 +1,52 @@
+typealias Graph = MutableMap<String, MutableSet<String>>
+
+fun main() {
+
+    fun List<String>.buildGraph(): Graph {
+        val nodeMap: Graph = mutableMapOf()
+        this.forEach {
+            val nodes = it.split("-")
+            nodeMap.computeIfAbsent(nodes[0]) { mutableSetOf() }.add(nodes[1])
+            nodeMap.computeIfAbsent(nodes[1]) { mutableSetOf() }.add(nodes[0])
+        }
+        return nodeMap
+    }
+
+    fun part1(graph: Graph): Int {
+        fun Graph.buildPath(path: List<String>, start: String, end: String): List<List<String>> {
+            if (start == end) return listOf(path + listOf(end))
+            val path = path + listOf(start)
+            fun canVisit(n: String) = n != n.lowercase() || !path.contains(n)
+            val neighbors = this[start]?.filter { canVisit(it) } ?: emptySet()
+            return neighbors.flatMap { buildPath(path, it, end) }
+        }
+        return graph.buildPath(emptyList(), "start", "end")
+            .onEach { println(it) }
+            .size
+    }
+
+    fun part2(graph: Graph): Int {
+        fun Graph.buildPath(path: List<String>, start: String, end: String): List<List<String>> {
+            if (start == end) return listOf(path + listOf(end))
+            val path = path + listOf(start)
+            fun canVisit(n: String): Boolean {
+                if (n == "start") return false
+                return n != n.lowercase()
+                        || !path.contains(n)
+                        || path.filter { it == it.lowercase() }
+                    .groupBy { it }
+                    .filter { it.value.size > 1 }.isEmpty()
+            }
+
+            val neighbors = this[start]?.filter { canVisit(it) } ?: emptySet()
+            return neighbors.flatMap { buildPath(path, it, end) }
+        }
+        return graph.buildPath(emptyList(), "start", "end")
+            .onEach { println(it) }
+            .size
+    }
+
+    val graph = readInput("day12").buildGraph()
+    println(part1(graph))
+    println(part2(graph))
+}
